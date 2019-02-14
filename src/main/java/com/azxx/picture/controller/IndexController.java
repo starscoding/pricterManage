cfd5fbd12baacf16f92551f4f81639a06007025a
xuzheng <hx0011001@hengxinyongli.com>
2019-02-14 20:12:40 +0800

    add main page

--- src/main/java/com/azxx/picture/controller/IndexController.java
+++ src/main/java/com/azxx/picture/controller/IndexController.java
@@ -33,4 +33,9 @@ public class IndexController {
     public String groupManage(){
         return  "/groupManage/groupManage";
     }
+
+    @RequestMapping(path = "/welcome/index",method = RequestMethod.GET)
+    public String welcome(){
+        return  "/welcome/welcome";
+    }
 }
